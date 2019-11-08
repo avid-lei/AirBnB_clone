94e750fd908eb61cc0164e84a70cda26a3854e20
Diva Lei <divalei@Divas-MacBook-Pro.local>
2019-11-07 22:21:17 -0800

    all

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
 """class Base Model"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
-    """ lass Base Model"""
-
+    """ class Base Model"""
     def __init__(self):
-        self.id = str(uuid.uuid4())
-      
-    @property
-    def created_at(self):
-        return self.created_at  
-
-    @created_at.setter
-    def created_at(self):
-       self.created_at = datetime.isoformat(datetime.datetime.now())
-
-    @property
-    def updated_at(self):
-        return self.updated_at
-
-    @updated_at.setter
-    def updated_at(self):
-        self.updated_at = datetime.isoformat(datetime.datetime.now())
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
-    
 
+    def save(self):
+        self.updated_at = datetime.now()
 
-
+    def to_dict(self):
+        d = self.__dict__
+        d['__class__'] = 'BaseModel'
+        d['updated_at'] = self.updated_at.isoformat()
+        d['created_at'] = self.created_at.isoformat()
+        return d
