c69e2304eb6fc1062ed541361535cd1cdd22288c
Diva Lei <divalei@Divas-MacBook-Pro.local>
2019-11-07 22:58:22 -0800

    #4

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,14 @@ from datetime import datetime
 
 class BaseModel:
     """ class Base Model"""
-    def __init__(self):
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) == 0:
+            self.id = uuid.uuid4()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            self.kwset(**kwargs)
+    
     def __str__(self):
         return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
 
@@ -23,3 +26,10 @@ class BaseModel:
         d['updated_at'] = self.updated_at.isoformat()
         d['created_at'] = self.created_at.isoformat()
         return d
+
+    def kwset(self, **kwargs):
+        for key, val in kwargs.items():
+            if not key == '__class__':
+                setattr(self, key, val)
+        self.created_at = datetime.strptime(self.created_at,'%Y-%m-%dT%H:%M:%S.%f')
+        self.updated_at = datetime.strptime(self.updated_at,'%Y-%m-%dT%H:%M:%S.%f')  
