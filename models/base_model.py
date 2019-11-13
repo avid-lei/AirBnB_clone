00e1fb3350cdefd010380fe282bb580a627dbb47
Diva Lei <divalei@Divas-MacBook-Pro.local>
2019-11-13 03:58:44 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """class Base Model"""
-
 from models import storage
 import uuid
 from datetime import datetime
@@ -11,13 +10,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ initialize Base Model """
-        if len(kwargs) == 0:
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            self.kwset(**kwargs)
+            for key, val in kwargs.items():
+                if not key == '__class__':
+                    setattr(self, key, val)
+            self.created_at = datetime.strptime(self.created_at,
+                                                '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(self.updated_at,
+                                                '%Y-%m-%dT%H:%M:%S.%f')
 
     def __str__(self):
         """string representation"""
@@ -39,10 +44,4 @@ class BaseModel:
 
     def kwset(self, **kwargs):
         """set attribute from kwargs"""
-        for key, val in kwargs.items():
-            if not key == '__class__':
-                setattr(self, key, val)
-        self.created_at = datetime.strptime(self.created_at,
-                                            '%Y-%m-%dT%H:%M:%S.%f')
-        self.updated_at = datetime.strptime(self.updated_at,
-                                            '%Y-%m-%dT%H:%M:%S.%f')
+        
