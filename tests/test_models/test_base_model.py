0169f28eadd100b38ca4a335c41bed47732c81af
Sasha <alex.mapatelian@gmail.com>
2019-11-13 04:19:38 +0000

    added tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -21,27 +21,27 @@ class TestMyBase(unittest.TestCase):
         self.assertTrue(self.base)
         self.assertTrue(self.new)
 
-# AssertionError: '2019-11-12T23:46:44.512804' != <class 'datetime.datetime'>
-#    def test_date(self):
-#        self.assertEqual(self.base.created_at, datetime)
-
     def test_is_str(self):
         """Testing if attributes are strings"""
         self.assertEqual(type(self.base.id), str)
-        self.assertEqual(type(self.base.created_at), str)
-        self.assertEqual(type(self.base.updated_at), str)
+
+    def test_datetime_type(self):
+        """Testing for datetime object"""
+        self.assertEqual(type(self.base.created_at), datetime)
+        self.assertEqual(type(self.base.updated_at), datetime)
 
     def test_class_inst(self):
         """Test if the instance is of BaseModel class"""
         self.assertEqual(type(self.base), BaseModel)
 
     def test_is_instance(self):
+        """Test for BaseModel class"""
         self.assertTrue(isinstance(self.base, BaseModel))
 
-    # AttributeError: 'str' object has no attribute 'isoformat'
-    #def test_is_dict(self):
-    #    dicty = self.base.to_dict()
-    #    self.assertEqual(type(dicty), dict)
+    def test_is_dict(self):
+        """Test for dictionary"""
+        dicty = self.base.to_dict()
+        self.assertEqual(type(dicty), dict)
 
     def test_is_updated(self):
         """Testing if value of datetime is updated"""
