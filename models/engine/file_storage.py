4e10225ce0225eb9aa2838bcf12e1274559989a0
Sasha <alex.mapatelian@gmail.com>
2019-11-13 01:20:59 +0000

    pep8 validation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,17 +10,21 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """Returns dictionary"""
         return self.__objects
 
     def new(self, obj):
+        """Assigns objects"""
         self.__objects['{}.{}'.format(obj.__class__.__name__,
                                       obj.id)] = obj.to_dict()
 
     def save(self):
+        """Writes an object to a file"""
         with open(self.__file_path, "w") as f:
             json.dump(self.__objects, f, default=str)
 
     def reload(self):
+        """Retrieves objects from a file"""
         try:
             with open(self.__file_path, "r") as f:
                 self.__objects = json.load(f)
