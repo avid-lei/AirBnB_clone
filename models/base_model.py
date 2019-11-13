335cf7b4cf811e59ce85c6e5ed42cfab27a690bf
Diva Lei <divalei@Divas-MacBook-Pro.local>
2019-11-12 18:03:21 -0800

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """class Base Model"""
+
 from models import storage
 import uuid
 from datetime import datetime
@@ -8,34 +9,39 @@ from datetime import datetime
 class BaseModel:
     """ class Base Model"""
     def __init__(self, *args, **kwargs):
+        """ initialize Base Model """
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-            #self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            #self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
-
         else:
             self.kwset(**kwargs)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """string representation"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
+        """save method"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """to dictionary method"""
         d = self.__dict__
-        d['__class__'] = self.__class__.__name__ 
-        d['updated_at'] = self.updated_at.isoformat() 
+        d['__class__'] = self.__class__.__name__
+        d['updated_at'] = self.updated_at.isoformat()
         d['created_at'] = self.created_at.isoformat()
         return d
 
     def kwset(self, **kwargs):
+        """set attribute from kwargs"""
         for key, val in kwargs.items():
             if not key == '__class__':
                 setattr(self, key, val)
-        self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-        self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+        self.created_at = datetime.strptime(self.created_at,
+                                            '%Y-%m-%dT%H:%M:%S.%f')
+        self.updated_at = datetime.strptime(self.updated_at,
+                                            '%Y-%m-%dT%H:%M:%S.%f')
