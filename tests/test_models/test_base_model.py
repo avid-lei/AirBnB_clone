d1211938636a84ae6ed97e0d7e2e46a41749c61e
Diva Lei <divalei@Divas-MacBook-Pro.local>
2019-11-13 21:08:39 -0800

    more tests for base

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
+import os
 
 class TestMyBase(unittest.TestCase):
 
@@ -13,6 +14,8 @@ class TestMyBase(unittest.TestCase):
     def tearDown(self):
         """Cleaning up"""
         del self.base
+        
+            
 
     def test_attributes(self):
         """Testing that attributes are set"""
@@ -21,14 +24,45 @@ class TestMyBase(unittest.TestCase):
         self.assertEqual(self.base.name, "Diva")
         self.assertEqual(self.base.num, 20)    
 
+    def has_attr(self):
+        """Test that instance has all attributes"""
+        self.assertTrue(hasattr(self.base, "id"))
+        self.assertTrue(hasattr(self.base, "create_at"))
+        self.assertTrue(hasattr(self.base, "updated_at"))
+        new = BaseModel()
+        new.name = "Diva"
+        new.number = 5
+        self.assertTrue(hasattr(new, "name"))
+        self.assertTrue(hasattr(new, "number"))
+
+        dicty = new.to_dict()
+
+        self.assertTrue('name' in dicty)
+        self.assertTrue('number' in dicty)
+    
+
     def test_is_created(self):
         """Testing if the instance was created"""
         self.assertEqual(self.base.__class__.__name__, 'BaseModel')
         self.assertTrue(self.base)
 
-    def test_is_str(self):
+    def test_str(self):
         """Testing if attributes are strings"""
         self.assertEqual(type(self.base.id), str)
+        new = BaseModel()
+        strrep = "[{}] ({}) {}".format(new.__class__.__name__,
+                                     new.id, new.__dict__)
+        self.assertEqual(new.__str__(), strrep)
+    
+    def test_updated(self):
+        """Test time is updated"""
+        old = self.base.updated_at
+        cre = self.base.created_at
+        self.assertEqual(old, self.base.updated_at)
+        self.assertEqual(cre, self.base.created_at)
+        self.base.save()
+        self.assertNotEqual(old, self.base.updated_at)
+        self.assertEqual(cre, self.base.created_at)
 
     def test_datetime_type(self):
         """Testing for datetime object"""
@@ -43,6 +77,30 @@ class TestMyBase(unittest.TestCase):
     def test_is_instance(self):
         """Test for BaseModel class"""
         self.assertTrue(isinstance(self.base, BaseModel))
+
+    def test_diff_instance(self):
+        """Test many instances"""
+        new1 = BaseModel()
+        new2 = BaseModel()
+        new3 = BaseModel()
+        new4 = BaseModel()
+
+        self.assertIsInstance(new1.id, str)
+        self.assertIsInstance(new1.updated_at, datetime)
+        self.assertIsInstance(new1.created_at, datetime)
+
+        self.assertIsInstance(new2.id, str)
+        self.assertIsInstance(new2.updated_at, datetime)
+        self.assertIsInstance(new2.created_at, datetime)
+
+        self.assertIsInstance(new3.id, str)
+        self.assertIsInstance(new3.updated_at, datetime)
+        self.assertIsInstance(new3.created_at, datetime)
+
+        self.assertIsInstance(new4.id, str)
+        self.assertIsInstance(new4.updated_at, datetime)
+        self.assertIsInstance(new4.created_at, datetime)
+
 
     def test_is_dict(self):
         """Test for dictionary"""
@@ -69,7 +127,7 @@ class TestMyBase(unittest.TestCase):
         self.assertIsNot(self.base.id, new.id)
     
     def test_passing_int(self):
-        """Testing what happens if numbers are passed into parameter"""
+        """Testing what happens if ints are passed into parameter"""
         new = BaseModel(5)
         self.assertIsInstance(new, BaseModel)
         self.assertIsInstance(new.created_at, datetime)
@@ -77,6 +135,7 @@ class TestMyBase(unittest.TestCase):
         self.assertIsInstance(new.id, str)
     
     def test_passing_float(self):
+        """Testing what happens if floats are passed into parameter"""
         new = BaseModel(6.1)
         self.assertIsInstance(new, BaseModel)
         self.assertIsInstance(new.created_at, datetime)
@@ -84,6 +143,7 @@ class TestMyBase(unittest.TestCase):
         self.assertIsInstance(new.id, str)
     
     def test_passing_list(self):
+        """Testing what happens if lists are passed into parameter"""
         new = BaseModel([1, 2, 3])
         self.assertIsInstance(new, BaseModel)
         self.assertIsInstance(new.created_at, datetime)
@@ -91,6 +151,7 @@ class TestMyBase(unittest.TestCase):
         self.assertIsInstance(new.id, str)
 
     def test_passing_string(self):
+        """Testing what happens if strings are passed into parameter"""
         new = BaseModel("Hello")
         self.assertIsInstance(new, BaseModel)
         self.assertIsInstance(new.created_at, datetime)
@@ -98,16 +159,32 @@ class TestMyBase(unittest.TestCase):
         self.assertIsInstance(new.id, str)
     
     def test_passing_dict(self):
+        """Testing what happens if dicts are passed into parameter"""
         new = BaseModel({})
         self.assertIsInstance(new, BaseModel)
         self.assertIsInstance(new.created_at, datetime)
         self.assertIsInstance(new.updated_at, datetime)
         self.assertIsInstance(new.id, str)
+
+    def test_save(self):
+        """Testing save and json file"""
+        new = BaseModel()
+        new.name = 'Diva'
+        idn = '{}.{}'.format(new.__class__.__name__,
+                                      new.id)
+        new.save()
+        with open("file.json", "r") as f:
+            fc = f.read()
+            self.assertTrue(idn in fc)
+            self.assertTrue('name": "Diva"' in fc)
+            self.assertTrue('updated_at' in fc)
+            self.assertTrue('created_at' in fc)
+        
+        os.remove("file.json")
+
+
     
 
-        
-
-       
 
 
 if __name__ == '__main__':
